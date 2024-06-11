6b8af4ba5ae6a26e0e77a8f4524f68b37941dafc
Atulbende <107322576+Atulbende@users.noreply.github.com>
2024-06-11 15:30:08 +0530

    Temp:WebSocket8

--- server/src/app.js
+++ server/src/app.js
@@ -15,8 +15,9 @@ const corsOptions = {
         'https://padamshri-dairy-git-master-atul-s-projects-b035b0ba.vercel.app',
         'https://padamshri-dairy-p2896etdc-atul-s-projects-b035b0ba.vercel.app',
         'https://padamshri-dairy-obxrrsxxw-atul-s-projects-b035b0ba.vercel.app',
-        'https://padamshri-dairy.vercel.app',
-        'https://padamshri-dairy-o7m5mstb6-atul-s-projects-b035b0ba.vercel.app'
+        'https://padamshri-dairy.vercel.app/login',
+        'https://padamshri-dairy-o7m5mstb6-atul-s-projects-b035b0ba.vercel.app',
+        'https://padamshri-dairy-git-master-atul-s-projects-b035b0ba.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'AppId'],
