8a0a448f688767e12f4b5724302961f4c9da44fe
Atulbende <107322576+Atulbende@users.noreply.github.com>
2024-06-11 13:41:22 +0530

    Temp:WebSocket

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Replace with your server URL
-const SOCKET_URL = 'http://localhost:8000'; 
+const SOCKET_URL = 'https://padamshri-dairy.onrender.com'; 
 
 const socket = io(SOCKET_URL, {
   path: '/dashboard/socket.io', // Ensure this matches the server path
